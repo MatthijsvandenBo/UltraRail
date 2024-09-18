97e7723cc5e071620a6fe63eb287a4eaaeab94c0
MitchelMA <32548@ma-web.nl>
2024-09-18 11:27:39 +0200

    interface voor het observeren van een field

---
+++ Source/UltraRail/Private/WorldGen/WaveFunctionCollapse/FieldObserver.cpp
@@ -0,0 +1,4 @@
+#include "WorldGen/WaveFunctionCollapse/FieldObserver.h"
+
+
+// Add default functionality here for any IFieldObserver functions that are not pure virtual.
---
+++ Source/UltraRail/Public/WorldGen/WaveFunctionCollapse/FieldObserver.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "FieldObserver.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UFieldObserver : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class ULTRARAIL_API IFieldObserver
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
