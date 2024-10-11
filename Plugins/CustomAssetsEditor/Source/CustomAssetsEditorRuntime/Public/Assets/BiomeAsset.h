a193df77d6ddbd9b03a16ad9c04703901bc2a339
MitchelMA <32548@ma-web.nl>
2024-10-11 13:11:10 +0200

    Setup voor custom asset runtime en custom asset editor

---
+++ Plugins/CustomAssetsEditor/Source/CustomAssetsEditorRuntime/Public/Assets/BiomeAsset.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BiomeAsset.generated.h"
+
+/**
+ * This is a custom asset written to be used for wave-function-collapse world generation
+ */
+UCLASS(BlueprintType)
+class CUSTOMASSETSEDITORRUNTIME_API UBiomeAsset : public UObject
+{
+ GENERATED_BODY()
+};
